6b456accb3a07faedf1ecc9ca6321b288a2b16bf
Brian Lego <legobw@gmail.com>
2020-06-27 00:10:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -37,7 +37,8 @@ def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
     binding.pry
-      names.each do |
+      names.each do |name|
+        if !final_array
     end
   end
 end
