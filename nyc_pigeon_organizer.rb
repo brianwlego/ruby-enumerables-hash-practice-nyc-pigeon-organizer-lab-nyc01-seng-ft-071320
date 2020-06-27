7d1b28e577a8879f6d44a76574a59dd7d5b9e3b6
Brian Lego <legobw@gmail.com>
2020-06-27 00:18:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -42,6 +42,8 @@ def nyc_pigeon_organizer(data)
           final_array[name] = {}
         end 
         if !final_array[name][key] 
+          final_array[name][key] = []
+        end  
       end  
     end
   end
